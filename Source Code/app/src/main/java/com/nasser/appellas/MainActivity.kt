4ccb8e1c12c2fd3f7f21f5ecf317eb4210d54336
jnasser513 <00245818@uca.edu.sv>
2021-10-09 17:59:08 -0600

    Adding bottom navigation menu layout

--- Source Code/app/src/main/java/com/nasser/appellas/MainActivity.kt
+++ Source Code/app/src/main/java/com/nasser/appellas/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.homepage_fragment)
+        setContentView(R.layout.blog_fragment)
     }
 }
--- Source Code/app/src/main/java/com/nasser/appellas/fragments/BlogFragment.kt
+++ Source Code/app/src/main/java/com/nasser/appellas/fragments/BlogFragment.kt
@@ -1,19 +1,83 @@
 package com.nasser.appellas.fragments
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
+import androidx.core.graphics.toColor
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.nasser.appellas.R
+import com.nasser.appellas.databinding.BlogFragmentBinding
 
 class BlogFragment: Fragment() {
+
+    private lateinit var mBinding: BlogFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        super.onCreateView(inflater, container, savedInstanceState)
+        mBinding = BlogFragmentBinding.inflate(inflater, container, false)
+
+        setUpListeners()
+
+        return mBinding.root
     }
+
+    private fun setUpListeners() {
+        setUpNavigation()
+    }
+
+    private fun setUpNavigation() {
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.page_blog -> {
+                    /** Ir a pagina de Blogs **/
+                    DrawableCompat.setTint(item.icon, R.color.white.toInt())
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.page_advisory -> {
+                    /** Ir a pagina de asesoria **/
+                    DrawableCompat.setTint(item.icon, R.color.white.toInt())
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.page_map -> {
+                    /** Ir a pagina de mapa **/
+                    DrawableCompat.setTint(item.icon, R.color.white.toInt())
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.page_information -> {
+                    /** Ir a pagina de informacion **/
+                    DrawableCompat.setTint(item.icon, R.color.white.toInt())
+                    return@OnNavigationItemSelectedListener true
+                }
+                else -> {
+                    DrawableCompat.setTint(item.icon, R.color.primary_color.toInt())
+                    return@OnNavigationItemSelectedListener false
+                }
+            }
+        }
+    }
+
+    /*
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.bottom_navigation_menu, menu)
+
+        for(i in 0 until menu.size()) {
+            var menuItem = menu.getItem(i)
+
+            val icon = menuItem.icon
+            if(icon != null){
+                DrawableCompat.setTint(icon, ContextCompat.getColor(requireContext(), R.color.primary_color))
+            }
+        }
+
+        return super.onCreateOptionsMenu(menu, inflater)
+    }*/
 
 }
